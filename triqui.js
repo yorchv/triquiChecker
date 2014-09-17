0dda5422809a12b42186791311bc90963c7002ba
jvivas <jvivas@climate.com>
2014-09-17 18:24:01 -0500

    Better structured code
    - Create the Triqui Class
    - Paint the console with the Triqui layout
    - Update the layout when the user sets a play
    - Created a 2 depth array cloning

--- triqui.js
+++ triqui.js
@@ -1,62 +1,128 @@
-var tick = {
-  a: [0, 0, 0],
-  b: [0, 0, 0],
-  c: [0, 0, 0]
-};
+/**
+ * @triqui.js
+ *
+ * Tic Tac Toe on the Console. Play against the computer
+ *
+ * @autor Jorge Vivas
+ * @copyright Jorge Vivas
+ * @license MIT: You are free to use and modify this code for any use, on the condition that this copyright notice remains.
+ */
 
-var resetTick = function () {
-  tick = {
-    a: [0, 0, 0],
-    b: [0, 0, 0],
-    c: [0, 0, 0]
-  };
-  return 'reseted';
-};
+// TODO / ADD DOCUMENTATION
+var Triqui = function() {
 
-var makeMove = function (line, col) {
-  var l = tick[line],
-    c = Number.parseInt(col) - 1,
-    checkWinner = function (tick) {
-      var winners = {
-        row: [
-          ['a0', 'a1', 'a2'],
-          ['b0', 'b1', 'b2'],
-          ['c0', 'c1', 'c2']
-        ],
-        col: [
-          ['a0', 'b0', 'c0'],
-          ['a1', 'b1', 'c1'],
-          ['a2', 'b2', 'c2']
-        ],
-        crs: [
-          ['a0', 'b1', 'c2'],
-          ['a2', 'b1', 'c0']
-        ]
-      }, opt, total = 0,
-        perm, coor, val;
+  var
 
-      for (var key in winners) {
-        opt = winners[key];
-        for (var i = 0; i < opt.length; i++) {
-          perm = opt[i];
-          total = 0;
-          for (var e = 0; e < perm.length; e++) {
-            coor = perm[e].split('');
-            val = tick[coor[0]][coor[1]];
-            total += val;
-          }
-          if (total === 3) {
-            return true;
+    // Starter game matrix
+    matrix = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ],
+
+    // Matrix of posible combinations winner
+    winnersMatrix = {
+      row: [
+        ['00', '01', '02'],
+        ['10', '11', '12'],
+        ['20', '21', '22']
+      ],
+      column: [
+        ['00', '10', '20'],
+        ['01', '11', '21'],
+        ['02', '12', '22']
+      ],
+      cross: [
+        ['00', '11', '22'],
+        ['02', '11', '20']
+      ]
+    },
+
+    // 2 Depth Array Cloning
+    cloneArr = function(arr) {
+      return arr.map(function(i) { return i.slice(); });
+    };
+
+  return {
+
+    matrix: cloneArr(matrix),
+
+    _matrix: cloneArr(matrix),
+
+    reset: function() {
+      this.matrix = cloneArr(this._matrix);
+      return 'Triqui Restarted';
+    },
+
+    set: function(row, column) {
+      var rw = this.validate(Number(row)),
+        col = this.validate(Number(column));
+
+      if (col === null || rw === null) {
+        return 'Use numbers from 0 to 2';
+      } else if (this.matrix[rw][col] === 1) {
+        return 'Already played row '+ row + ', column ' + column;
+      }
+
+      this.matrix[rw][col] = 1;
+      this.ink();
+
+      if (this.checkWinner()) {
+        return 'You have WON!';
+      }
+
+      return 'You played: row '+ row + ', column ' + column;
+    },
+
+    validate: function(number) {
+      var max = this._matrix.length,
+        min = 0;
+
+      return number <= max && number >= min ? number : null;
+    },
+
+    ink: function() {
+      //Show the triqui on the console
+      var i, e, output = [], mtx = this.matrix, length = mtx.length;
+      output.push('  | 0 | 1 | 2 |\n');
+      for (i = 0; i < length; i++) {
+        output.push(i + ' | ');
+        for (e = 0; e < mtx[i].length; e++) {
+          e && output.push(' ');
+          output.push(mtx[i][e] ? 'X |' : '- |');
+        }
+        output.push('\n');
+      }
+
+      console.log(output.join(''));
+    },
+
+    checkWinner: function () {
+      var key, matrix = this.matrix, winners = winnersMatrix, opt, total = 0,
+        perm, coor, val, i, e;
+
+      for (key in winners) {
+        if (winners.hasOwnProperty(key)) {
+          opt = winners[key];
+          for (i = 0; i < opt.length; i++) {
+            perm = opt[i];
+            total = 0;
+            for (e = 0; e < perm.length; e++) {
+              coor = perm[e].split('');
+              val = matrix[Number(coor[0])][Number(coor[1])];
+              total += val;
+            }
+            if (total === 3) {
+              return true;
+            }
           }
         }
       }
 
       return false;
-    };
-
-  if (!l[c]) {
-    l[c] = 1;
-  }
-
-  return checkWinner(tick);
+    }
+  };
 };
+
+// TODO Closure
+triqui = new Triqui();
